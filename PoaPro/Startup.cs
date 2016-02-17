945bf92297efaba65a1d1f983e5217962c6f2ca1
denisg-g <denisg-g@hotmail.com>
2016-02-17 13:45:05 -0600

    commit inicial

---
+++ PoaPro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PoaPro.Startup))]
+namespace PoaPro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
